d1a6db1be8252d5405e4db89b4ca03081e959288
wulu <wulu90@outlook.com>
2025-02-22 16:03:30 +0800

    1-2

--- 01.cpp
+++ 01.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <map>
 #include <print>
+#include <set>
 #include <vector>
 
 using namespace std;
@@ -8,7 +9,7 @@ using namespace std;
 enum class turn { L, R };
 enum class direction { N, E, S, W };
 
-void next_coord(pair<int, int>& curr, direction& dir, turn t, int dist) {
+vector<pair<int, int>> next_coord(pair<int, int>& curr, direction& dir, turn t, int dist) {
     static const array<array<int, 2>, 4> deltas{{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}};
     static const map<pair<direction, turn>, size_t> delta_inx{
         {{direction::N, turn::R}, 1}, {{direction::E, turn::R}, 2}, {{direction::S, turn::R}, 3}, {{direction::W, turn::R}, 0},
@@ -21,12 +22,21 @@ void next_coord(pair<int, int>& curr, direction& dir, turn t, int dist) {
 
     auto& [x, y] = curr;
     auto inx     = delta_inx.at({dir, t});
+
+    vector<pair<int, int>> path;
+
+    for (int i = 1; i <= dist; ++i) {
+        path.emplace_back(x + deltas[inx][0] * i, y + deltas[inx][1] * i);
+    }
+
     x += deltas[inx][0] * dist;
     y += deltas[inx][1] * dist;
     dir = dir_next.at({dir, t});
+
+    return path;
 }
 
-void part1() {
+void part1_2() {
     ifstream input("input/input01");
     vector<pair<turn, int>> turn_dist_vec;
     for (char c, d; input; c = input.get()) {
@@ -50,9 +60,30 @@ void part1() {
     }
 
     println("{}", abs(curr.first) + abs(curr.second));
+
+    curr = {0, 0};
+    dir  = direction::N;
+    set<pair<int, int>> visited;
+
+    for (auto [t, d] : turn_dist_vec) {
+        auto path  = next_coord(curr, dir, t, d);
+        bool twice = false;
+        for (auto p : path) {
+            if (visited.contains(p)) {
+                twice = true;
+                println("{}", abs(p.first) + abs(p.second));
+                break;
+            } else {
+                visited.insert(p);
+            }
+        }
+        if (twice) {
+            break;
+        }
+    }
 }
 
 int main() {
-    part1();
+    part1_2();
     return 0;
 }
