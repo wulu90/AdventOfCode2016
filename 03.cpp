a222269d9adecfee7be8fdb3475a4003554648f6
wulu <wulu90@outlook.com>
2025-02-22 17:22:06 +0800

    3-2

--- 03.cpp
+++ 03.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <print>
+#include <vector>
 
 using namespace std;
 
@@ -22,7 +23,38 @@ void part1() {
     println("{}", valid_count);
 }
 
+uint triangle_in_column(const vector<uint>& vec) {
+    uint valid = 0;
+    for (size_t i = 0; i < vec.size() - 2; i += 3) {
+        if (valid_triangle(vec[i], vec[i + 1], vec[i + 2])) {
+            ++valid;
+        }
+    }
+    return valid;
+}
+
+void part2() {
+    ifstream input("input/input03");
+    uint a, b, c;
+    vector<vector<uint>> vec_vec(3);
+    while (input.good()) {
+        input >> a;
+        input >> b;
+        input >> c;
+        vec_vec[0].push_back(a);
+        vec_vec[1].push_back(b);
+        vec_vec[2].push_back(c);
+    }
+    uint valid_count = 0;
+    for (size_t i = 0; i < 3; ++i) {
+        valid_count += triangle_in_column(vec_vec[i]);
+    }
+
+    println("{}", valid_count);
+}
+
 int main() {
     part1();
+    part2();
     return 0;
 }
