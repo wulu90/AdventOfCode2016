8a8a0ac9a8d234df7a62cdcb0764a24b18fea862
wulu <wulu90@outlook.com>
2025-02-23 12:08:12 +0800

    10-2

--- 10.cpp
+++ 10.cpp
@@ -6,12 +6,6 @@
 
 using namespace std;
 
-struct bot {
-    uint v1;
-    uint v2;
-    uint vc;    // value count; 0,1,2
-};
-
 tuple<uint, uint, uint> parse_num(const string& str) {
     tuple<uint, uint, uint> res;
     get<0>(res) = 123;
@@ -51,7 +45,7 @@ void part1() {
         if (line.starts_with("value")) {
             id_bot[b].insert(a);
             unvalued_bot.insert(b);
-        } else {
+        } else if (!line.contains("output")) {
             bot_give_vec.push_back({a, b, c});
             unvalued_bot.insert(a);
             unvalued_bot.insert(b);
@@ -87,7 +81,71 @@ void part1() {
     }
 }
 
+tuple<string, string, string> parse2(const string& str) {
+    auto [a, b, c] = parse_num(str);
+    string sa, sb, sc;
+    if (str.contains("low to bot")) {
+        sb = format("b{}", b);
+    }
+    if (str.contains("low to output")) {
+        sb = format("o{}", b);
+    }
+
+    if (str.contains("high to bot")) {
+        sc = format("b{}", c);
+    }
+    if (str.contains("high to output")) {
+        sc = format("o{}", c);
+    }
+    sa = format("b{}", a);
+    return {sa, sb, sc};
+}
+
+void part2() {
+    ifstream input("input/input10");
+    map<string, set<uint>> id_bot;
+    vector<tuple<string, string, string>> bot_give_vec;
+    set<string> unvalued_bot;
+    for (string line; getline(input, line);) {
+        if (line.starts_with("value")) {
+            auto [a, b, c] = parse_num(line);
+            auto botname   = format("b{}", b);
+            id_bot[botname].insert(a);
+            unvalued_bot.insert(botname);
+        } else {
+            auto [sa, sb, sc] = parse2(line);
+            bot_give_vec.push_back({sa, sb, sc});
+            unvalued_bot.insert(sa);
+            unvalued_bot.insert(sb);
+            unvalued_bot.insert(sc);
+        }
+    }
+
+    bool find = false;
+    while (!unvalued_bot.empty()) {
+        for (auto [a, b, c] : bot_give_vec) {
+            if (id_bot[a].size() == 2) {
+                id_bot[b].insert(*id_bot[a].begin());
+                id_bot[c].insert(*id_bot[a].rbegin());
+            }
+        }
+
+        for (auto& [id, bo] : id_bot) {
+            if ((id.starts_with("b") && bo.size() == 2) || (id.starts_with("o") && bo.size() == 1)) {
+                unvalued_bot.erase(id);
+            }
+        }
+
+        if ((!id_bot["o0"].empty()) && (!id_bot["o1"].empty()) && (!id_bot["o2"].empty())) {
+            println("{},{},{},{}", id_bot["o0"], id_bot["o1"], id_bot["o2"],
+                    *id_bot["o0"].begin() * *id_bot["o1"].begin() * *id_bot["o2"].begin());
+            break;
+        }
+    }
+}
+
 int main() {
     part1();
+    part2();
     return 0;
 }
