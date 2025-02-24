bb2b057142cd64b59c27c3dc94021d8a76a448ec
wulu <wulu90@outlook.com>
2025-02-24 22:55:20 +0800

    20-2

--- 20.cpp
+++ 20.cpp
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <fstream>
-#include <ranges>
 #include <vector>
 
 using namespace std;
@@ -13,29 +12,60 @@ void part1() {
         input >> a;
         input.get();    // skip '-'
         input >> b;
-        blacklist.emplace_back(a, b);
+        blacklist.push_back(minmax(a, b));
     }
     blacklist.pop_back();    // because of last blank line
 
     sort(blacklist.begin(), blacklist.end());
 
-    auto [low, high] = blacklist.front();
-    if (low != 0) {
-        println("0");
-        return;
-    }
+    uint32_t bound = 0;
 
-    for (auto [l, h] : blacklist | views::drop(1)) {
-        if (l > high + 1) {
-            println("{}", high + 1);
+    for (auto [l, h] : blacklist) {
+        if (l > bound + 1) {
+            println("{}", bound + 1);
             break;
         } else {
-            high = max(high, h);
+            bound = max(bound, h);
         }
     }
 }
 
+void part2() {
+    ifstream input("input/input20");
+    uint32_t a, b;
+    vector<pair<uint32_t, uint32_t>> blacklist;
+    while (input.good()) {
+        input >> a;
+        input.get();    // skip '-'
+        input >> b;
+
+        blacklist.push_back(minmax(a, b));
+    }
+    blacklist.pop_back();    // because of last blank line
+
+    sort(blacklist.begin(), blacklist.end());
+
+    uint32_t bound = 0;
+    uint32_t count = 0;
+
+    for (auto [l, h] : blacklist) {
+        if (l > bound + 1) {
+            count += l - bound - 1;
+            bound = h;
+        } else {
+            bound = max(bound, h);
+        }
+
+        if (bound == UINT32_MAX) {
+            break;
+        }
+    }
+    count += UINT32_MAX - bound;
+    println("{}", count);
+}
+
 int main() {
     part1();
+    part2();
     return 0;
 }
